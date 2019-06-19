0c1a66a5740a0dcff448231ea17295a0b3de54b8
Eli Zoller <zoller.eli@gmail.com>
2019-06-19 12:55:38 -0700

    add createVersion test

---
+++ test/CreateVersionTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Islandora\Chullo;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use Islandora\Chullo\FedoraApi;
+use \RuntimeException;
+
+class CreateVersionTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers  Islandora\Chullo\FedoraApi::modifyResource
+     * @uses    GuzzleHttp\Client
+     */
+    public function testReturns201withVersions()
+    {
+        $mock = new MockHandler(
+            [
+            new Response(200, ['Link' => '<http://localhost:8080/rest/path/to/resource/fcr:versions>;rel="timemap"']),
+            new Response(201, ['Location' => "SOME URI"])
+            ]
+        );
+
+        $handler = HandlerStack::create($mock);
+        $guzzle = new Client(['handler' => $handler]);
+        $api = new FedoraApi($guzzle);
+
+        $result = $api->createVersion('');
+    }
+
+    public function testThrowsExceptionWithoutTimemapUri()
+    {
+        $mock = new MockHandler(
+            [
+            new Response(200, []),
+            new Response(201, ['Location' => "SOME URI"])
+            ]
+        );
+
+        $handler = HandlerStack::create($mock);
+        $guzzle = new Client(['handler' => $handler]);
+        $api = new FedoraApi($guzzle);
+
+        $this->expectException(\RuntimeException::class);
+        $result = $api->createVersion('');
+    }
+}
